5b2869c075df709911be599864c2b01fab8a903a
archemisc <archemisc@kin.puffy>
2019-09-21 06:08:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tiedonanto - SpawnFest 2019
+
+Tiedonanto (who means "communication" in Finnish), is a project to
+create a new way to interract with remote end-point in Erlang.
+
+## How to compile
+
+```
+rebar3 compile
+```
+
+## How to use
+
+```
+rebar3 shell
+```
+
+## About the SpawnFest
---
+++ src/tiedonanto_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc tiedonanto public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(tiedonanto_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    tiedonanto_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
